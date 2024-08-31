b180386350c8cc201cc71daac18863c51977e2cf
TchazeuL <loicmeyong17@gmail.com>
2024-08-31 18:04:50 +0100

    creating different repositories

--- src/repositories/InventaireRepository.ts
+++ src/repositories/InventaireRepository.ts
@@ -1,7 +1,15 @@
 import InventaireImpl from "../models/Inventaire";
 
 interface InventaireRepository {
+
     save(inventaire: InventaireImpl): void;
+
+    findAll() : Array<InventaireImpl> ; 
+
+    findById(id: number): InventaireImpl ;
+
+    update(id: number, inventaire: InventaireImpl) : InventaireImpl;
+
 }
 
 export default InventaireRepository;
